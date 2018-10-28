a6ca5c6afe5392f824920f80013a863649892a68
shashank shekhar <shekharshashank922@gmail.com>
2018-10-29 00:29:47 +0530

    fianl commit

--- producthunt/producthunt/settings.py
+++ producthunt/producthunt/settings.py
@@ -115,9 +115,8 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+# https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'producthunt/static/')
